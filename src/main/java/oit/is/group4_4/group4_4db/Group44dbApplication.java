1d07bfa33aecc070a0bbc4ed0fc64507274be035
e1b18012 <e1b18012.oit.ac.jp>
2020-10-20 15:45:55 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/group4_4/group4_4db/Group44dbApplication.java
@@ -0,0 +1,13 @@
+package oit.is.group4_4.group4_4db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group44dbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group44dbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/group4_4/group4_4db/Group44dbApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group4_4.group4_4db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group44dbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
